9642e6a626763d10eccffc662de7c3ee126112b3
User2573 <71093251+User2573@users.noreply.github.com>
2022-02-14 15:33:19 -0500

    Create fcolor.hpp

---
+++ image/fcolor.hpp
@@ -0,0 +1,29 @@
+#ifndef FCOLOR_H
+#define FCOLOR_H
+
+#include <iostream>
+#include "../math/vector.hpp"
+
+
+
+class FColor
+{
+public:
+	unsigned char r, g, b;
+
+	FColor();
+
+	FColor(const unsigned char _x);
+
+	FColor(const unsigned char _r, const unsigned char _g, const unsigned char _b);
+
+	FColor(const Vector _v);
+
+
+
+	friend std::ostream& operator << (std::ostream& os, const FColor& color);
+};
+
+
+
+#endif
