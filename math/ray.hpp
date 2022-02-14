be114049e6850ab921e4e6db9e3dc688b125af77
User2573 <71093251+User2573@users.noreply.github.com>
2022-02-14 15:07:35 -0500

    Create ray.hpp

---
+++ math/ray.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef RAY_HPP
+#define RAY_HPP
+
+#include <iostream>
+#include "vector.hpp"
+
+
+
+class Ray
+{
+public:
+	Point origin;
+	Vector direction;
+
+
+
+	Ray();
+
+	Ray(const Vector _direction);
+
+	Ray(const Point _origin, const Vector _direction);
+
+
+
+	Point pointAt(double t) const;
+
+	Ray& normalizeDirection();
+
+	Ray& lookAt(const Point& p);
+
+
+
+	friend std::ostream& operator << (std::ostream &os, const Ray& ray);
+};
+
+
+
+#endif
