a3bbab3c760be5ec34931adf1bf44686d2fdf33c
User2573 <71093251+User2573@users.noreply.github.com>
2022-02-14 15:08:09 -0500

    Create ray.cpp

---
+++ math/ray.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "ray.hpp"
+#include "vector.hpp"
+
+
+
+Ray::Ray() : origin(), direction() {}
+
+Ray::Ray(const Vector _direction) : origin(), direction(_direction)
+{
+	normalizeDirection();
+}
+
+Ray::Ray(const Point _origin, const Vector _direction) : origin(_origin), direction(_direction)
+{
+	normalizeDirection();
+}
+
+
+
+Point Ray::pointAt(double t) const
+{
+	return origin + t*direction;
+}
+
+Ray& Ray::normalizeDirection()
+{
+	if (length2(direction) != 1.0) {
+		direction.normalize();
+	}
+	return *this;
+}
+
+Ray& Ray::lookAt(const Point& p)
+{
+	direction = p - origin;
+	return normalizeDirection();
+}
+
+std::ostream& operator << (std::ostream& os, const Ray& ray)
+{
+	os << "Ray{\n"
+	"    origin:    " << ray.origin << ",\n"
+	"    direction: " << ray.direction << "\n"
+	"}";
+	return os;
+}
